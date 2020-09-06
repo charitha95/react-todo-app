dcf4255cb0ce4e5d354936ad850dfc3bbb3a3865
charitha95 <>
2020-09-06 14:09:55 +0530

    chore(antd): add ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { Button } from "antd";
+import "./app.less";
+
 function App() {
   return (
     <section className="card">
+      <Button type="primary">Button</Button>
       <p className="text-red-300">
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
